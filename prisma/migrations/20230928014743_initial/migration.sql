a8c42901bbc4ac623ce9fe752c5ecb96c502083f
devnoob547 <danielalvespessoa3006@gmail.com>
2023-09-27 22:48:32 -0300

    connect prisma

---
+++ prisma/migrations/20230928014743_initial/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Enrollment" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Enrollment_pkey" PRIMARY KEY ("id")
+);
