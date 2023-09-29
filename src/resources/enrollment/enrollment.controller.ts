a7d4009488235c5a81a84c359e7730e19dc254f6
devnoob547 <danielalvespessoa3006@gmail.com>
2023-09-29 09:53:41 -0300

    remove typing from the create enrollment function

--- src/resources/enrollment/enrollment.controller.ts
+++ src/resources/enrollment/enrollment.controller.ts
@@ -1,14 +1,13 @@
 import { Body, Controller, Post, Get, Delete, Param } from '@nestjs/common';
 import { EnrollmentService } from './enrollment.service';
 import { CreateEnrollmentDTO } from 'src/dtos/create-enrollment.dto';
-import { Enrollment } from '@prisma/client';
 
 @Controller('enrollment')
 export class EnrollmentController {
   constructor(private readonly enrollmentService: EnrollmentService) {}
 
   @Post()
-  createEnrollment(@Body() body: CreateEnrollmentDTO): Promise<Enrollment> {
+  createEnrollment(@Body() body: CreateEnrollmentDTO) {
     return this.enrollmentService.createEnrollment(body);
   }
 
